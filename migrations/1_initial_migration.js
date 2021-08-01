6235609f688d1099ff47331aaa4b8d9c9a710ba9
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-01 18:52:51 +0530

    Unit tests for ERC721 smart contract deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
+// migrations are used to add smart contracts to the blockchain
+
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
 };
--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.5.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ERC721Full.sol";
 
-contract Color is ERC721 {}
+contract Color is ERC721Full {
+  constructor() public ERC721Full("Color", "COLOR") {}
+}
