51594cdbf7b483d793a60a8acc7fd15c35cb5a94
Yash Jaiswal <yashjaiswal1@outlook.com>
2021-08-02 11:28:20 +0530

    Add mint function for NFTs

--- test/Color.test.js
+++ test/Color.test.js
@@ -12,7 +12,7 @@ contract("Color", (accounts) => {
   });
 
   describe("deployment", async () => {
-    it("Deployed successfully", async () => {
+    it("Deploys successfully", async () => {
       const address = contract.address;
       console.log("Contract address: " + address);
       assert.notEqual(address, "");
@@ -29,6 +29,28 @@ contract("Color", (accounts) => {
     it("has a symbol", async () => {
       const symbol = await contract.symbol();
       assert.equal(symbol, "COLOR");
+    });
+  });
+
+  describe("minting", async () => {
+    it("Creates a new token", async () => {
+      const result = await contract.mint("#EC058E");
+      const totalSupply = await contract.totalSupply();
+
+      // Success
+      assert.equal(totalSupply, 1);
+      //   console.log(result);
+      const event = result.logs[0].args;
+      assert.equal(event.tokenId.toNumber(), 1, "ID is correct");
+      assert.equal(
+        event.from,
+        "0x0000000000000000000000000000000000000000",
+        "from is correct"
+      );
+      assert.equal(event.to, accounts[0], "to is correct");
+
+      // Failure: cannot mint same color twice
+      await contract.mint("#EC058E").should.be.rejected;
     });
   });
 });
